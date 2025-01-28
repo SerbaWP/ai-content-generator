970403cb45d5e7a69f0a50db7e40bfd3d412823d
SerbaWP <serbawp@gmail.com>
2025-01-28 19:19:09 +0700

    Update class-template-manager.php

--- includes/class-template-manager.php
+++ includes/class-template-manager.php
@@ -5,16 +5,15 @@ class AI_Content_Template_Manager {
   }
 
   public function save_template($template_id, $data) {
+    $templates = $this->get_templates();
+    
     if (empty($data['name']) || empty($data['prompt'])) {
-      return new WP_Error('invalid_template', 'Template name and prompt are required.');
+      return new WP_Error('validation', 'Name and prompt are required');
     }
 
-    $templates = $this->get_templates();
     $templates[$template_id] = [
       'name' => sanitize_text_field($data['name']),
-      'prompt' => sanitize_textarea_field($data['prompt']),
-      'post_type' => sanitize_text_field($data['post_type']),
-      'post_status' => sanitize_text_field($data['post_status'])
+      'prompt' => sanitize_textarea_field($data['prompt'])
     ];
     
     update_option('ai_content_templates', $templates);
@@ -23,9 +22,6 @@ class AI_Content_Template_Manager {
 
   public function delete_template($template_id) {
     $templates = $this->get_templates();
-    if (!isset($templates[$template_id])) {
-      return new WP_Error('invalid_template', 'Template does not exist');
-    }
     unset($templates[$template_id]);
     update_option('ai_content_templates', $templates);
   }
